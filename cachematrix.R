0f4df8ecbfacc161df0119ae98c1b1258d6f43a7
ChrisCamphausen <chris@camphausen.net.au>
2017-01-26 17:17:37 -0500

    CKC Coursera Assigment

--- cachematrix.R
+++ cachematrix.R
@@ -1,34 +1,54 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a special matrix object, 
+## which contains a list of functions to set and get a matrix 
+## i.e. store in current environment as "c_x", 
+## and set and get the inverted matrix stored as "inverse_x"
+##
 
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
   set <- function(y) {
-    x <<- y
-    m <<- NULL
+    c_x <<- y
+    inverse_x <<- NULL
   }
-  get <- function() x
-  setmean <- function(mean) m <<- mean
-  getmean <- function() m
+  set(x)
+  get <- function() c_x
+  setinverse <- function(inverse) inverse_x <<- inverse
+  getinverse <- function() inverse_x
   list(set = set, get = get,
-       setmean = setmean,
-       getmean = getmean)
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve returns the inverse of a matrix
+## if a previous call of cacheSolve
+## If the cached inverse is available, cacheSolve retrieves it, while if
+## not, it computes, caches, and returns it.
 
 cacheSolve <- function(x, ...) {
 ## Return a matrix that is the inverse of 'x'
-  m <- x$getmean()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+  inv_x <- x$getinverse()
+  if(!is.null(inv_x)) {
+    message("getting cached inverse matrix")
+    return(inv_x)
   }
   data <- x$get()
-  m <- mean(data, ...)
-  x$setmean(m)
-  m
+  inv_x <- solve(data, ...)
+  x$setinverse(inv_x)
+  inv_x
 }
+
+
+## Test workflow
+## Create matrix: x<-matrix(c(2,3,5,6,7,8,10,12,13),nrow=3,ncol=3)
+## make cache matrix: cm<-makeCacheMatrix(x)
+## cm is a list with 4 values, the functions $set, $get, $setinverse, $getinverse 
+## makeCacheMatrix calls cm$set(x) to store x in the global variable c_x
+## and make inverse_x<<-NULL 
+## cacheSolve(cm) either retrieves the cached value of inverse_x via $getinverse()
+## or calculates inv_X and stores it in inverse_x via $setinverse(inv_x)
+## the second call of cacheSolve(cm) displays a message about the cached matrix  
+
+
